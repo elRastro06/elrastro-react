698fd4b6a858e538c980e3c9f17533fe5f3b7cf8
JCamacho4 <jorgecamagarc@gmail.com>
2023-11-21 20:03:28 +0100

    prepare to work with branches

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,112 @@
 import { useState } from 'react'
 import './assets/css/App.css'
 
-import Home from "./components/Home/home.jsx";
+import Chat from "./components/Chats/chat.jsx";
 import Chats from "./components/Chats/chats.jsx";
+
+import Login from "./components/Login/login.jsx";
+
 import AboutUs from "./components/AboutUs/aboutus.jsx";
+
+import Products from "./components/Products/products.jsx";
+import Product from "./components/Products/product.jsx";
+
+import Profile from "./components/Profile/profile.jsx";
+import MyProfile from "./components/Profile/myprofile.jsx";
+
+import Map from "./components/Map/map.jsx"
+
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    // errorElement: <PaginaError />,
-  },
-  {
-    path: "/:id/chats",
-    element: <Chats />,
-    // errorElement: <PaginaError />,
-  },
-  {
-    path: "/about-us",
-    element: <AboutUs />,
-    // errorElement: <PaginaError />,
-  }
+    /**
+     * ====================
+     *       Products
+     * ====================
+     */
+    {
+        path: "/",
+        element: <Products />,
+        // errorElement: <PaginaError />,
+    },
+    {
+        path: "/product/:id",
+        element: <Product />,
+        // errorElement: <PaginaError />,
+    },
+
+    /**
+     * ====================
+     *        Chats
+     * ====================
+     */
+    {
+        path: "/chats",
+        element: <Chats />,
+        // errorElement: <PaginaError />,
+    },
+    {
+        path: "/chats/:id",
+        element: <Chat />,
+        // errorElement: <PaginaError />,
+    },
+    /**
+     * ====================
+     *        Login
+     * ====================
+     */
+    {
+        path: "/login",
+        element: <Login />,
+        // errorElement: <PaginaError />,
+    },
+    /**
+     * ====================
+     *       About Us
+     * ====================
+     */
+    {
+        path: "/aboutus",
+        element: <AboutUs />,
+        // errorElement: <PaginaError />,
+    },
+    /**
+     * ====================
+     *       Profile
+     * ====================
+     */
+    {
+        path: "/my-profile",
+        element: <MyProfile />,
+        // errorElement: <PaginaError />,
+    },
+    {
+        path: "/profile/:id",
+        element: <Profile />,
+        // errorElement: <PaginaError />,
+    },
+    /**
+     * ====================
+     *         Map
+     * ====================
+     */
+    {
+        path: "/map",
+        element: <Map />,
+        // errorElement: <PaginaError />,
+    },
 ]);
 
 function App() {
 
-  return (
-    <>
-      <div>
-        <RouterProvider router={router} />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <RouterProvider router={router} />
+            </div>
+        </>
+    )
 }
 
 export default App
