35b356a7c175068e931655bc170643b4f39dcd76
4ntoniofr <afr012240@uma.es>
2023-11-25 17:34:22 +0100

    Lista productos

--- src/components/Products/products.jsx
+++ src/components/Products/products.jsx
@@ -1,13 +1,46 @@
-import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-// import "../../assets/css/Products.css";
+import "../../assets/styles/products.css";
 
 export default function Products() {
-    return(
-        <div>
-            <h1>Products</h1>
-        </div>
-    )
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5001/v1/")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Products</h1>
+
+      <div className="products">
+        {products.map((product) => (
+          <div className="product" key={product._id}>
+            <div className="product-info">
+              <h2 className="product-name">{product.name}</h2>
+              <h3 className="product-price">{product.price}€</h3>
+              <p className="product-description">{product.description}</p>
+            </div>
+            {product.images ? (
+              <div className="product-image">
+                <img src={product.images[0].url} alt={product.name} />
+              </div>
+            ) : (
+              <div className="product-image">
+                <img src="no_image.png" alt="No image available" />
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
