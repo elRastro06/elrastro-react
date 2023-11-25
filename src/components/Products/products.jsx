c5e3c90f427d96bf4807b9bc925f2e158bd50fee
4ntoniofr <afr012240@uma.es>
2023-11-25 18:43:27 +0100

    Filtrado y bids

--- src/components/Products/products.jsx
+++ src/components/Products/products.jsx
@@ -5,6 +5,8 @@ import "../../assets/styles/products.css";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
+  const [bids, setBids] = useState([]);
+  const [filter, setFilter] = useState(""); // New state for filtering
 
   useEffect(() => {
     axios
@@ -17,29 +19,74 @@ export default function Products() {
       });
   }, []);
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:5002/v1/")
+      .then((response) => {
+        setBids(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const getHighestBid = (productId) => {
+    const vals = bids
+      .filter((bid) => bid.productId === productId)
+      .map((bid) => bid.amount);
+    if (vals.length === 0) {
+      return 0;
+    }
+    return Math.max(...vals); // Spread operator to get the maximum value
+  };
+
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div>
-      <h1>Products</h1>
+      <h1 className="products-title">El Rastro</h1>
+
+      <div className="products-filter">
+        <input
+          className="products-filter-input"
+          type="text"
+          placeholder="Filter by name"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+      </div>
 
       <div className="products">
-        {products.map((product) => (
-          <div className="product" key={product._id}>
-            <div className="product-info">
-              <h2 className="product-name">{product.name}</h2>
-              <h3 className="product-price">{product.price}€</h3>
-              <p className="product-description">{product.description}</p>
+        {filteredProducts.map((product) => {
+          const bid = getHighestBid(product._id);
+
+          return (
+            <div className="product" key={product._id}>
+              <div className="product-info">
+                <h2 className="product-name">{product.name}</h2>
+                <h3 className="product-price">
+                  Original price: {product.price}€
+                </h3>
+                <p className="product-description">{product.description}</p>
+              </div>
+              {product.images ? (
+                <div className="product-image">
+                  <img src={product.images[0].url} alt={product.name} />
+                </div>
+              ) : (
+                <div className="product-image">
+                  <img src="no_image.png" alt="No image available" />
+                </div>
+              )}
+              <div className="product-bids">
+                <p>Highest Bid: {bid !== 0 ? `${bid}€` : "No bids yet"}</p>
+                <button className="product-bid-button">Place a bid</button>
+              </div>
             </div>
-            {product.images ? (
-              <div className="product-image">
-                <img src={product.images[0].url} alt={product.name} />
-              </div>
-            ) : (
-              <div className="product-image">
-                <img src="no_image.png" alt="No image available" />
-              </div>
-            )}
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
