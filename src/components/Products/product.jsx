ca596d3bbd489f1c561e42c468d5f4199ef43653
artachojf <pepe.artacho.martin@uma.es>
2023-12-03 20:30:21 +0100

    Timer, delete and modify buttons

--- src/components/Products/product.jsx
+++ src/components/Products/product.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 
 import "../../assets/styles/product.css";
 import productServices from "../../services/productServices";
@@ -17,26 +17,61 @@ export default function Product() {
     const [selectedImage, setSelectedImage] = useState(0);
     const [owner, setOwner] = useState({});
     const [newBid, setNewBid] = useState(0.0);
+    const [timeLeft, setTimeLeft] = useState({
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+    });
 
-    const fetchData = async () => {
-        const productData = await productServices.getProduct(productId);
-
-        if (productData._id) {
-            setProduct(productData);
-        } else {
-            navigate("/");
-        }
-
-        const bidsData = await bidServices.getBids(productId);
-        setBids(bidsData);
-
-        const ownerData = await clientServices.getClient(productData.userID);
-        setOwner(ownerData);
-    }
+    const loggedUserId = "654f4c3cf99b7fddc72edd1b";    //TEMPORAL HASTA IMPLEMENTAR LOGIN
+    //654f4c1bf99b7fddc72edd19 consola
+    //654f4c2bf99b7fddc72edd1a
+    //654f4c3cf99b7fddc72edd1b silla
 
     useEffect(() => {
+        const fetchData = async () => {
+            const productData = await productServices.getProduct(productId);
+
+            if (productData._id) {
+                setProduct(productData);
+            } else {
+                navigate("/");
+                return;
+            }
+
+            const bidsData = await bidServices.getBids(productId);
+            setBids(bidsData);
+
+            const ownerData = await clientServices.getClient(productData.userID);
+            setOwner(ownerData);
+        }
+
         fetchData().catch(console.error);
     }, []);
+
+    useEffect(() => {
+        const calculateTimeLeft = (startDate) => {
+            const date = Date.parse(startDate);
+            const today = new Date().getTime();
+            const maxDiff = 7 * 24 * 60 * 60 * 1000;
+
+            const diff = date + maxDiff - today;
+
+            setTimeLeft({
+                days: Math.floor(diff / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((diff / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((diff / 1000 / 60) % 60),
+                seconds: Math.floor((diff / 1000) % 60)
+            });
+        }
+
+        if (product.date != undefined) {
+            const interval = setInterval(() => calculateTimeLeft(product.date), 1000);
+
+            return () => clearInterval(interval);
+        }
+    }, [product]);
 
     const prevImage = () => {
         setSelectedImage(Math.abs(selectedImage - 1) % product.images.length);
@@ -47,9 +82,17 @@ export default function Product() {
     }
 
     const addBid = async (event) => {
+        event.preventDefault();
+
+        if(timeLeft.seconds < 0 || timeLeft.minutes < 0 || timeLeft.hours < 0
+            || timeLeft.days < 0) {
+                alert("Bid is over. You can not make a new bid");
+                return;
+            }
+
         const bid = {
             amount: parseFloat(newBid),
-            userId: 0, //userId
+            userId: loggedUserId,
             productId: product._id
         };
 
@@ -58,7 +101,7 @@ export default function Product() {
         if (res.error != undefined || res.information != undefined) {
             alert("Bid not inserted. Price must be higher than current highest bid");
         } else {
-            navigate(0);
+            navigate(0);    //refresh
         }
     }
 
@@ -75,12 +118,41 @@ export default function Product() {
         return date.toLocaleDateString(undefined, options); //'es-ES'
     }
 
+    const timeLeftStr = () => {
+        return timeLeft.days + "d " + timeLeft.hours + "h " + timeLeft.minutes +
+            "m " + timeLeft.seconds + "s";
+    }
+
+    const deleteProduct = async () => {
+        if (bids.length != 0) alert("No se puede eliminar un producto con pujas");
+        else {
+            await productServices.deleteProduct(productId);
+            navigate("/");
+        }
+    }
+
+    const modifyProduct = () => {
+
+    }
+
     return (
         <div className="product-container">
             <section className="product-image-container">
                 <img className="product-page-image" src={product.images[selectedImage].url} alt="Product Image"></img>
                 <button className="product-image-button prev" onClick={() => prevImage()}>&#8249;</button>
                 <button className="product-image-button next" onClick={() => nextImage()}>&#8250;</button>
+                <div className="product-owner-options">
+                    {loggedUserId == product.userID &&
+                        <>
+                            <button id="product-modify" onClick={() => modifyProduct()}>
+                                <span className="material-icons">edit</span>
+                            </button>
+                            <button id="product-delete" onClick={() => deleteProduct()}>
+                                <span className="material-icons">delete</span>
+                            </button>
+                        </>
+                    }
+                </div>
             </section>
 
             <section className="product-info-container">
@@ -88,9 +160,10 @@ export default function Product() {
                     <h3>{product.name}</h3>
                     <p>{product.description}</p>
                     <p className="product-price">{product.price}€</p>
+                    <span>{timeLeftStr()}</span>
                 </section>
                 <div className="product-owner">
-                    <span>Owner</span>
+                    <Link to={`/profile/${owner._id}`}>{owner.email}</Link>
                     <img src="https://avatars.githubusercontent.com/u/100372552?s=96&v=4"></img>
                 </div>
                 <h4>Bids made</h4>
@@ -100,9 +173,12 @@ export default function Product() {
                         <p>{handleDate(bid.date)}</p>
                     </div>
                 })}
-
-                <input type="number" step={0.1} value={newBid} onChange={(event) => setNewBid(event.target.value)}></input>
-                <button className="bid-form-btn" onClick={addBid}>Make bid</button>
+                <form className="bid-form">
+                    <input type="number" step={0.1} value={newBid} onChange={(event) => setNewBid(event.target.value)}></input>
+                    <button className="bid-form-btn" onClick={addBid} disabled={loggedUserId == product.userID}>
+                        <span className="material-icons">gavel</span>
+                    </button>
+                </form>
             </section>
         </div>
     )
--- src/services/productServices.js
+++ src/services/productServices.js
@@ -5,6 +5,16 @@ const getProduct = async (id) => {
     return response.data;
 };
 
-const productServices = { getProduct };
+const deleteProduct = async (id) => {
+    const response = await axios.delete(`http://localhost:5001/v1/${id}`);
+    return response.data;
+}
+
+const modifyProduct = async (id, body) => {
+    const response = await axios.put(`http://localhost:5001/v1/${id}`, body);
+    return response.data;
+}
+
+const productServices = { getProduct, deleteProduct, modifyProduct };
 
 export default productServices;
