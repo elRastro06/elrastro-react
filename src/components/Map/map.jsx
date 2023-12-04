2545119c7cb1c4f447bad7ddaaabe5f2396a3715
Valentín <valentingr080@uma.es>
2023-12-04 16:53:03 +0100

    if only one product it doest show a carousel

--- src/components/Map/map.jsx
+++ src/components/Map/map.jsx
@@ -15,18 +15,19 @@ const Map = (props) => {
 
   useEffect(() => {
     setRadius(props.radius);
-    const clientsWithProducts = props.clients.map(client => ({
+    const clientsWithProducts = props.clients.map((client) => ({
       ...client,
-      products: props.products.filter(product => product.userID === client._id)
+      products: props.products.filter(
+        (product) => product.userID === client._id
+      ),
     }));
-    
+
     setClientsWithProducts(clientsWithProducts);
   }, [props.products]);
 
   return (
     <div className={props.className}>
-      <div
-      style={{height: "100%", width:"100%", border: "solid 1px black"}}>
+      <div style={{ height: "100%", width: "100%", border: "solid 1px black" }}>
         <MapContainer
           center={defaultPosition}
           zoom={10}
@@ -36,60 +37,96 @@ const Map = (props) => {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
-          <Circle center={defaultPosition} radius={radius*1000} />
-          {clientsWithProducts.map((cliente) => (
-            cliente.products && cliente.products.length > 0 && (
-              // each client has a marker on the map
-              <Marker
-              key={cliente._id}
-              position={[
-                cliente.location.coordinates[1],
-                cliente.location.coordinates[0],
-              ]}
-              >
-              <Popup>
-                <div className="popup-container">
-                  {loading ? (
-                    <p>Loading products...</p>
-                  ) : (
-                    <div className="carousel-container">
-                        <Carousel>
-                          {cliente.products.map((product) => (
-                            <Carousel.Item key={product._id}>
-                              <div className="map-product-info">
-                                <a href={`/product/${product._id}`}>
-                                  <h3>{product.name}</h3>
-                                  <img
-                                    className="map-product-image"
-                                    src={
-                                      product.images
-                                        ? product.images[0].url
-                                        : "no_image.png"
-                                    }
-                                    alt="Product Image"
-                                  />
-                                </a>
-                                <p>
-                                  <strong>Description:</strong>{" "}
-                                  {product.description}
-                                  <br></br>
-                                  <strong>Price:</strong> {product.price}
-                                  <br></br>
-                                  <strong>Date:</strong>{" "}
-                                  {new Date(product.date).toLocaleDateString()}
-                                  <br></br>
-                                </p>
-                              </div>
-                            </Carousel.Item>
-                          ))}
-                        </Carousel>
+          <Circle center={defaultPosition} radius={radius * 1000} />
+          {clientsWithProducts.map(
+            (cliente) =>
+              cliente.products &&
+              cliente.products.length > 0 && (
+                // each client has a marker on the map
+                <Marker
+                  key={cliente._id}
+                  position={[
+                    cliente.location.coordinates[1],
+                    cliente.location.coordinates[0],
+                  ]}
+                >
+                  <Popup>
+                    <div className="popup-container">
+                      {loading ? (
+                        <p>Loading products...</p>
+                      ) : (
+                        <div className="carousel-container">
+                          {cliente.products.length === 1 ? (
+                            // Render just the single product without Carousel
+                            <div className="map-product-info">
+                              <a href={`/product/${cliente.products[0]._id}`}>
+                                <h3>{cliente.products[0].name}</h3>
+                                <img
+                                  className="map-product-image"
+                                  src={
+                                    cliente.products[0].images
+                                      ? cliente.products[0].images[0].url
+                                      : "no_image.png"
+                                  }
+                                  alt="Product Image"
+                                />
+                              </a>
+                              <p>
+                                <strong>Description:</strong>{" "}
+                                {cliente.products[0].description}
+                                <br></br>
+                                <strong>Price:</strong>{" "}
+                                {cliente.products[0].price}
+                                <br></br>
+                                <strong>Date:</strong>{" "}
+                                {new Date(
+                                  cliente.products[0].date
+                                ).toLocaleDateString()}
+                                <br></br>
+                              </p>
+                            </div>
+                          ) : (
+                            // Render Carousel for multiple products
+                            <Carousel>
+                              {cliente.products.map((product) => (
+                                <Carousel.Item key={product._id}>
+                                  <div className="map-product-info">
+                                    <a href={`/product/${product._id}`}>
+                                      <h3>{product.name}</h3>
+                                      <img
+                                        className="map-product-image"
+                                        src={
+                                          product.images
+                                            ? product.images[0].url
+                                            : "no_image.png"
+                                        }
+                                        alt="Product Image"
+                                      />
+                                    </a>
+                                    <p>
+                                      <strong>Description:</strong>{" "}
+                                      {product.description}
+                                      <br></br>
+                                      <strong>Price:</strong> {product.price}
+                                      <br></br>
+                                      <strong>Date:</strong>{" "}
+                                      {new Date(
+                                        product.date
+                                      ).toLocaleDateString()}
+                                      <br></br>
+                                    </p>
+                                  </div>
+                                </Carousel.Item>
+                              ))}
+                            </Carousel>
+                          )}
+                        </div>
+                      )}
                     </div>
-                  )}
-                </div>
-              </Popup>
-              </Marker>
-            )
-          ))}
+                  </Popup>
+                </Marker>
+              )
+          )}
         </MapContainer>
       </div>
     </div>
