b02241fd95519a6bac1fd3d8d651b41221321d97
VALEN-PC\arion <valentingr080@gmail.com>
2023-12-17 11:23:43 +0100

    profile working

--- src/components/AboutUs/aboutus.jsx
+++ src/components/AboutUs/aboutus.jsx
@@ -6,43 +6,70 @@ import axios from "axios";
 import "../../assets/styles/aboutus.css";
 
 export default function AboutUs({ userLogged }) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (userLogged == undefined) {
-            alert("Login needed. Please login and try again");
-            navigate("/login");
-        }
-    }, []);
+  useEffect(() => {
+    if (userLogged == undefined) {
+      alert("Login needed. Please login and try again");
+      navigate("/login");
+    }
+  }, []);
 
-    return (
-        <div class="about-us">
-            <h1>About Us</h1>
-            <p>We are a public store where anyone can sell or buy items, using a bidding system to determine the price. We also offer anonymous chat with vendors, so you can ask questions and negotiate without having to reveal your identity.</p>
+  return (
+    <div className="about-us">
+      <h1>About Us</h1>
+      <p>
+        We are a public store where anyone can sell or buy items, using a
+        bidding system to determine the price. We also offer anonymous chat with
+        vendors, so you can ask questions and negotiate without having to reveal
+        your identity.
+      </p>
 
-            <h3>Our Mission</h3>
-            <p>Our mission is to provide a fair and transparent marketplace for people to sell and buy goods. We believe that everyone should have the opportunity to make money from their unwanted items, and that buyers should be able to find the best deals on the products they need.</p>
+      <h3>Our Mission</h3>
+      <p>
+        Our mission is to provide a fair and transparent marketplace for people
+        to sell and buy goods. We believe that everyone should have the
+        opportunity to make money from their unwanted items, and that buyers
+        should be able to find the best deals on the products they need.
+      </p>
 
-            <h3>Why Choose Us?</h3>
-            <ul>
-                <li>Fair and transparent bidding system</li>
-                <li>Anonymous chat with vendors</li>
-                <li>Wide variety of items to choose from</li>
-                <li>Secure payment processing</li>
-                <li>Excellent customer support</li>
-            </ul>
+      <h3>Why Choose Us?</h3>
+      <ul>
+        <li>Fair and transparent bidding system</li>
+        <li>Anonymous chat with vendors</li>
+        <li>Wide variety of items to choose from</li>
+        <li>Secure payment processing</li>
+        <li>Excellent customer support</li>
+      </ul>
 
-            <h3>How It Works</h3>
-            <p>To sell an item, simply create an account and list your item. You can set a starting price and bidding end date. Buyers can then browse and bid on your item. The highest bidder at the end of the auction will win the item.</p>
+      <h3>How It Works</h3>
+      <p>
+        To sell an item, simply create an account and list your item. You can
+        set a starting price and bidding end date. Buyers can then browse and
+        bid on your item. The highest bidder at the end of the auction will win
+        the item.
+      </p>
 
-            <p>To buy an item, simply browse our listings and find an item you're interested in. You can then place a bid on the item. If your bid is the highest at the end of the auction, you will win the item.</p>
+      <p>
+        To buy an item, simply browse our listings and find an item you're
+        interested in. You can then place a bid on the item. If your bid is the
+        highest at the end of the auction, you will win the item.
+      </p>
 
-            <h3>Anonymous Chat</h3>
-            <p>We offer anonymous chat with vendors so you can ask questions and negotiate without having to reveal your identity. This can be helpful if you're not sure if you're interested in an item, or if you want to try to get a better price.</p>
+      <h3>Anonymous Chat</h3>
+      <p>
+        We offer anonymous chat with vendors so you can ask questions and
+        negotiate without having to reveal your identity. This can be helpful if
+        you're not sure if you're interested in an item, or if you want to try
+        to get a better price.
+      </p>
 
-            <h3>Contact Us</h3>
-            <p>If you have any questions or need help with anything, please don't hesitate to contact us. We're here to help you have a great experience on our platform.</p>
-        </div>
-
-    )
+      <h3>Contact Us</h3>
+      <p>
+        If you have any questions or need help with anything, please don't
+        hesitate to contact us. We're here to help you have a great experience
+        on our platform.
+      </p>
+    </div>
+  );
 }
--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -7,231 +7,268 @@ import ReviewForm from "./reviewForm";
 import loginServices from "../../services/loginServices";
 
 export default function Profile({ userLogged }) {
-    const navigate = useNavigate();
-    let { id } = useParams();
+  const navigate = useNavigate();
+  let { id } = useParams();
 
-    const [user, setUser] = useState({});
-    const [products, setProducts] = useState([]);
-    const [reviews, setReviews] = useState([]);
-    const [userLocation, setUserLocation] = useState("");
-    const [reviewers, setReviewers] = useState([]);
-    const [avg, setAvg] = useState([]);
+  const [user, setUser] = useState({});
+  const [products, setProducts] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const [userLocation, setUserLocation] = useState("");
+  const [reviewers, setReviewers] = useState([]);
+  const [avg, setAvg] = useState([]);
 
-    const [commonSale, setCommonSale] = useState({});
-    const [commonReview, setCommonReview] = useState({});
+  const [commonSale, setCommonSale] = useState({});
+  const [commonReview, setCommonReview] = useState({});
 
-    const clientsConn = import.meta.env.CLIENTS != undefined ? import.meta.env.CLIENTS : "localhost";
-    const productsConn = import.meta.env.PRODUCTS != undefined ? import.meta.env.PRODUCTS : "localhost";
-    const reviewsConn = import.meta.env.REVIEWS != undefined ? import.meta.env.REVIEWS : "localhost";
+  const clientsConn =
+    import.meta.env.CLIENTS != undefined
+      ? import.meta.env.CLIENTS
+      : "localhost";
+  const productsConn =
+    import.meta.env.PRODUCTS != undefined
+      ? import.meta.env.PRODUCTS
+      : "localhost";
+  const reviewsConn =
+    import.meta.env.REVIEWS != undefined
+      ? import.meta.env.REVIEWS
+      : "localhost";
 
-    useEffect(() => {
-        if (userLogged == undefined) {
-            alert("Login needed. Please login and try again");
-            navigate("/login");
-            return;
-        }
+  useEffect(() => {
+    if (userLogged == undefined) {
+      alert("Login needed. Please login and try again");
+      navigate("/login");
+      return;
+    }
 
-        if (!id) {
-            id = userLogged._id;
-        }
-        // Fetch user data by ID
-        axios
-            .get(`http://${clientsConn}:5000/v1/${id}`)
-            .then((response) => {
-                setUser(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    if (!id) {
+      id = userLogged._id;
+    }
+    // Fetch user data by ID
+    axios
+      .get(`http://${clientsConn}:5000/v1/${id}`, {
+        headers: {
+          Authorization: userLogged.oauthToken,
+        },
+      })
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-        // Fetch potential reviewer data
-        axios
-            .get(`http://${clientsConn}:5000/v1/`)
-            .then((response) => {
-                setReviewers(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    // Fetch potential reviewer data
+    axios
+      .get(`http://${clientsConn}:5000/v1/`, {
+        headers: {
+          Authorization: userLogged.oauthToken,
+        },
+      })
+      .then((response) => {
+        setReviewers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-        // Fetch products data for the user
-        axios
-            .get(`http://${productsConn}:5001/v2/?userID=${id}`)
-            .then((response) => {
-                setProducts(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    // Fetch products data for the user
+    axios
+      .get(`http://${productsConn}:5001/v2/?userID=${id}`, {
+        headers: {
+          Authorization: userLogged.oauthToken,
+        },
+      })
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-        //Fetch reviews data for the user
-        axios
-            .get(`http://${clientsConn}:5000/v2/${id}/reviews`)
-            .then((response) => {
-                setReviews(response.data);
-                setEditModes(response.data.map((review) => false));
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    //Fetch reviews data for the user
+    axios
+      .get(`http://${clientsConn}:5000/v2/${id}/reviews`, {
+        headers: {
+          Authorization: userLogged.oauthToken,
+        },
+      })
+      .then((response) => {
+        setReviews(response.data);
+        setEditModes(response.data.map((review) => false));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-        //Fetch average review value
-        axios
-            .get(`http://${clientsConn}:5000/v2/${id}/reviewsavg`)
-            .then((response) => {
-                setAvg(response.data.reviewAvg);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    //Fetch average review value
+    axios
+      .get(`http://${clientsConn}:5000/v2/${id}/reviewsavg`, {
+        headers: {
+          Authorization: userLogged.oauthToken,
+        },
+      })
+      .then((response) => {
+        setAvg(response.data.reviewAvg);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-        //Fetch data for conditions on review interaction
-        axios
-            .get(`http://${productsConn}:5001/v2/${userLogged._id}/${id}`)
-            .then((response) => {
-                setCommonSale(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        axios
-            .get(`http://${reviewsConn}:5008/v2/${userLogged._id}/${id}`)
-            .then((response) => {
-                setCommonReview(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, [id]);
+    //Fetch data for conditions on review interaction
+    axios
+      .get(`http://${productsConn}:5001/v2/${userLogged._id}/${id}`, {
+        headers: {
+          Authorization: userLogged.oauthToken,
+        },
+      })
+      .then((response) => {
+        setCommonSale(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    axios
+      .get(`http://${reviewsConn}:5008/v2/${userLogged._id}/${id}`, {
+        headers: {
+          Authorization: userLogged.oauthToken,
+        },
+      })
+      .then((response) => {
+        setCommonReview(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
 
-    useEffect(() => {
-        // Fetch user location
-        if (!user.location) return;
-        axios
-            .get(
-                `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${user.location.coordinates[1]}&longitude=${user.location.coordinates[0]}&localityLanguage=es`
-            )
-            .then((response) => {
-                setUserLocation(response.data.city + ", " + response.data.countryName);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }, [user]);
+  useEffect(() => {
+    // Fetch user location
+    if (!user.location) return;
+    axios
+      .get(
+        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${user.location.coordinates[1]}&longitude=${user.location.coordinates[0]}&localityLanguage=es`
+      )
+      .then((response) => {
+        setUserLocation(response.data.city + ", " + response.data.countryName);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [user]);
 
-    const getReviewer = (ReviewerId) => {
-        return reviewers.find(
-            (reviewer) =>
-                reviews.find((review) => review.reviewerID === ReviewerId)
-                    .reviewerID === reviewer._id
-        );
-    };
-
-    return (
-        <div className="profile-first-container">
-            <h1 className="profile-title">{user.name}</h1>
-            <div className="profile-info">
-                <div className="profile-image">
-                    <img src={"/user.jpg"} alt={user.name} />
-                </div>
-                <div className="profile-details">
-                    <div className="profile-email">
-                        <span className="material-icons">mail</span>
-                        <p>
-                            <strong>Email: </strong> {user.email}
-                        </p>
-                    </div>
-                    <div className="profile-location">
-                        <span className="material-icons">location_on</span>
-                        <p>
-                            <strong>Location: </strong> {userLocation}
-                        </p>
-                    </div>
-                    <div className="profile-valoraciones">
-                        <span className="material-icons">star</span>
-                        <p>
-                            <strong>Review Average: </strong>{" "}
-                            {avg ? avg / 5 : "No reviews yet"}
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <h2 className="profile-products-title">Products</h2>
-            {Array.isArray(products) && products.length > 0 && (
-                <div className="profile-container">
-                    {products.map((product) => (
-                        <div
-                            className="profile-product"
-                            key={product._id}
-                            onClick={() => {
-                                navigate("/product/" + product._id);
-                            }}
-                        >
-                            {product.images ? (
-                                <div className="profile-product-image">
-                                    <img src={product.images[0].url} alt={product.name} />
-                                </div>
-                            ) : (
-                                <div className="profile-product-image">
-                                    <img src="/no_image.png" alt="No image available" />
-                                </div>
-                            )}
-                            <div className="profile-product-info">
-                                <h3 className="profile-product-name">{product.name}</h3>
-                                <p className="profile-product-description">
-                                    {product.description}
-                                </p>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            )}
-
-            <h2 className="profile-reviews-title">
-                {" "}
-                <span className="material-icons">star</span> Reviews
-            </h2>
-
-            {Array.isArray(reviews) &&
-                reviews.length > 0 &&
-                (commonSale._id == undefined ? (
-                    <div className="profile-review-interaction">
-                        You have not yet sold to or bought an item from this user. Do so in
-                        order to rate him.
-                    </div>
-                ) : commonReview._id == undefined ? (
-                    <div className="profile-container">
-                        <ReviewForm reviewedID={id} />
-                    </div>
-                ) : (
-                    <div className="profile-review-interaction">
-                        You have already reviewed this user
-                    </div>
-                ))}
-
-            {Array.isArray(reviews) && reviews.length > 0 && (
-                <div className="profile-container-reviews">
-                    {reviews.map((review) => {
-                        const reviewer = getReviewer(review.reviewerID);
-
-                        return (
-                            <div className="review-container" key={review._id}>
-                                <ReviewForm
-                                    passedReview={review}
-                                    reviewedID={id}
-                                    reviewer={reviewer}
-                                    userLogged={userLogged}
-                                />
-                            </div>
-                        );
-                    })}
-                </div>
-            )}
-            {Array.isArray(reviews) && reviews.length == 0 && (
-                <div className="profile-review-interaction">
-                    There are not reviews for this user yet
-                </div>
-            )}
-        </div>
+  const getReviewer = (ReviewerId) => {
+    return reviewers.find(
+      (reviewer) =>
+        reviews.find((review) => review.reviewerID === ReviewerId)
+          .reviewerID === reviewer._id
     );
+  };
+
+  return (
+    <div className="profile-first-container">
+      <h1 className="profile-title">{user.name}</h1>
+      <div className="profile-info">
+        <div className="profile-image">
+          <img src={"/user.jpg"} alt={user.name} />
+        </div>
+        <div className="profile-details">
+          <div className="profile-email">
+            <span className="material-icons">mail</span>
+            <p>
+              <strong>Email: </strong> {user.email}
+            </p>
+          </div>
+          <div className="profile-location">
+            <span className="material-icons">location_on</span>
+            <p>
+              <strong>Location: </strong> {userLocation}
+            </p>
+          </div>
+          <div className="profile-valoraciones">
+            <span className="material-icons">star</span>
+            <p>
+              <strong>Review Average: </strong>{" "}
+              {avg ? avg / 5 : "No reviews yet"}
+            </p>
+          </div>
+        </div>
+      </div>
+      <h2 className="profile-products-title">Products</h2>
+      {Array.isArray(products) && products.length > 0 && (
+        <div className="profile-container">
+          {products.map((product) => (
+            <div
+              className="profile-product"
+              key={product._id}
+              onClick={() => {
+                navigate("/product/" + product._id);
+              }}
+            >
+              {product.images ? (
+                <div className="profile-product-image">
+                  <img src={product.images[0].url} alt={product.name} />
+                </div>
+              ) : (
+                <div className="profile-product-image">
+                  <img src="/no_image.png" alt="No image available" />
+                </div>
+              )}
+              <div className="profile-product-info">
+                <h3 className="profile-product-name">{product.name}</h3>
+                <p className="profile-product-description">
+                  {product.description}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+
+      <h2 className="profile-reviews-title">
+        {" "}
+        <span className="material-icons">star</span> Reviews
+      </h2>
+
+      {Array.isArray(reviews) &&
+        reviews.length > 0 &&
+        (commonSale._id == undefined ? (
+          <div className="profile-review-interaction">
+            You have not yet sold to or bought an item from this user. Do so in
+            order to rate him.
+          </div>
+        ) : commonReview._id == undefined ? (
+          <div className="profile-container">
+            <ReviewForm reviewedID={id} />
+          </div>
+        ) : (
+          <div className="profile-review-interaction">
+            You have already reviewed this user
+          </div>
+        ))}
+
+      {Array.isArray(reviews) && reviews.length > 0 && (
+        <div className="profile-container-reviews">
+          {reviews.map((review) => {
+            const reviewer = getReviewer(review.reviewerID);
+
+            return (
+              <div className="review-container" key={review._id}>
+                <ReviewForm
+                  passedReview={review}
+                  reviewedID={id}
+                  reviewer={reviewer}
+                  userLogged={userLogged}
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
+      {Array.isArray(reviews) && reviews.length == 0 && (
+        <div className="profile-review-interaction">
+          There are not reviews for this user yet
+        </div>
+      )}
+    </div>
+  );
 }
